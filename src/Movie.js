b0ab9c8c5ee3134242cf9b6cfb65d7275c19a86d
nohseungrae <ca10205@naver.com>
2019-11-04 00:45:12 +0900

    repaire

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css"
-const Movie = ({ id, year, title, summary, poster, genres }) => {
+const Movie = ({ year, title, summary, poster, genres }) => {
   return (
     <div className="movie">
       <img src={poster} alt={title} />
